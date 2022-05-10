a7134dd3bc795aa2c6f517bd212c996e9a1abe83
Igor Talankin <italankin@gmail.com>
2022-05-10 10:57:55 +0500

    Added test's utility function to create state from string

--- app/src/test/java/com/italankin/fifteen/game/Util.java
+++ app/src/test/java/com/italankin/fifteen/game/Util.java
@@ -1,5 +1,8 @@
 package com.italankin.fifteen.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Util {
     private static final int[] TMP = new int[2];
 
@@ -7,5 +10,14 @@ public final class Util {
         TMP[0] = index % width;
         TMP[1] = index / width;
         return TMP;
+    }
+
+    public static List<Integer> stateFromString(String value) {
+        String[] split = value.split("[\\s,;]+");
+        List<Integer> result = new ArrayList<>(split.length);
+        for (String s : split) {
+            result.add(Integer.parseInt(s));
+        }
+        return result;
     }
 }
